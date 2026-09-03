36d7562d8300506c30e6acab08cfb07633278e8c
RosiRoses <rosina.pansino003@gmail.com>
2026-09-04 03:23:17 +0500

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   base: '/Apple-Website/',
 
